eac23765b249241e5d5d26bc5ff5314f020a3e6e
shakibosd <syednazmusshakib94@gmail.com>
2025-04-28 00:25:06 +0600

    feat(visitWebsite): successfully completed

--- visitWebsite/urls.py
+++ visitWebsite/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import VisitorCountAPIView
 
 urlpatterns = [
-    path('', VisitorCountAPIView.as_view(), name='visitor-count'),
+    path('', VisitorCountAPIView.as_view(), name='visitor-counts'),
 ]
