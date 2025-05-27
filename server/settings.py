ac47414659f6cd920944de298c19da971d55f5ae
najmus-shakib01 <syednajmusshakib130@gmail.com>
2025-05-28 04:14:30 +0600

    feat(CORS_ALLOWED_ORIGINS): frontend live link changes

--- server/settings.py
+++ server/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
- "https://jamiyahusainiya.netlify.app",
+ "https://jamiyahusainiya.vercel.app",
 ]
 
 # Application definition
