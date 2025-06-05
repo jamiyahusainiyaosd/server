34354eba7a6cce597b4964a9cf208a84cddf5748
najmus-shakib01 <syednajmusshakib130@gmail.com>
2025-06-05 18:19:02 +0600

    feat(contact): apiview chages

--- contact/views.py
+++ contact/views.py
@@ -1,12 +1,11 @@
-from django.core.mail import send_mail
-from django.conf import settings
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import status
-import logging
 from .serializers import ContactFormSerializer
-
+from django.core.mail import EmailMessage
+import logging
 logger = logging.getLogger(__name__)
+import os
 
 class ContactFormView(APIView):
     def post(self, request):
@@ -17,36 +16,47 @@ class ContactFormView(APIView):
             phone = serializer.validated_data['phone']
             message = serializer.validated_data['message']
 
-            subject = f"Contact Form Submission from {name}"
+            subject = f"New message from {name} ({email})"
             email_message = f"""
-            Name : {name}
-            Email : {email}
-            Phone : {phone}
+            You have received a new message from your website:
 
+            Name: {name}
+            Email: {email}
+            Phone: {phone}
+            
             Message:
-            {message.strip()}
-            """.strip()
-
+            {message}
+            """
+            from_email = os.environ.get("EMAIL")
+           
             try:
-                send_mail(
-                    subject,
-                    email_message,
-                    settings.EMAIL_HOST_USER, 
-                    ['jamiyahusainiya1@gmail.com'], 
-                    fail_silently=False  
+                admin_email_obj = EmailMessage(
+                    subject=subject,
+                    body=email_message,
+                    from_email=from_email,
+                    to=[from_email],
+                    reply_to=[email]
                 )
-                logger.info(f"Email sent successfully from {email} (Phone: {phone})")
+                admin_email_obj.send(fail_silently=False)
 
-                return Response({"message": "Email Sent Successfully!"}, status=status.HTTP_200_OK)
+                user_email_obj = EmailMessage(
+                    subject="জামিয়া হুসাইনিয়া মাদ্রাসার সাথে যোগাযোগের জন্য ধন্যবাদ!",
+                    body=f"""হাই {name},\n\n
+                    যোগাযোগের জন্য ধন্যবাদ। আমরা আপনার বার্তা পেয়েছি।\n\n
+                    আপনার দেওয়া তথ্য:
+                    নাম: {name}
+                    ইমেইল: {email}
+                    ফোন: {phone}\n\n
+                    আপনার মেসেজটি ছিল:\n{message}""",
+                    from_email=from_email,
+                    to=[email]
+                )
+                user_email_obj.send(fail_silently=False)
+
+                return Response({"message": "Email sent successfully! Please Check Your Email"}, status=status.HTTP_200_OK)
 
             except Exception as e:
-                logger.error(f"Email sending failed: {repr(e)}")
-                return Response(
-                    {"error": f"Failed to send email: {str(e)}"},
-                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
-                )
+                logger.error(f"Email sending failed: {str(e)}")
+                return Response({"error": "Failed to send email."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        return Response(
-            {"error": "Invalid data", "details": serializer.errors},
-            status=status.HTTP_400_BAD_REQUEST
-        )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
